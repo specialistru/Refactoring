d57716429bcdb8930b3eaa1465ab8776cd37099a
Специалист <213274794+specialistru@users.noreply.github.com>
2025-06-16 13:54:14 +0300

    Update main.c

--- lesson/main.c
+++ lesson/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 // Объявление функции из example.c
-void run_example(int);
+void run_example(int number);
 
 int main(void) {
     for (int i = 1; i <= 300; i++) {
-        printf("=== Пример %03d ===\n", i);
         run_example(i);
-        printf("\n");
     }
     return 0;
 }
